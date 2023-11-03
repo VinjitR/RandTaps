53506e5ec158f1261591edcd810796765c175bc7
VinjitR <sregulag@usc.edu>
2023-11-02 20:32:36 -0700

    mongo connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const { ApolloServer} = require('apollo-server');
 const gql = require('graphql-tag');
-
+const mongoose = require('mongoose');
+const { MONGODB } = require('./config.js');
 
 const typeDefs = gql`
     type Query{
@@ -11,7 +12,7 @@ const typeDefs = gql`
 
 const resolvers = {
     Query : {
-        sayHi: () =>'HelloWorld!'
+        sayHi: () =>'HelloWorld!!!!'
     }
 }
 
@@ -20,7 +21,13 @@ const server = new ApolloServer({
     resolvers
 });
 
-server.listen({ port: 5000})
+mongoose.connect(MONGODB, {useNewUrlParser: true})
+    .then(()=>{
+        console.log("MongoDB connected")
+        return server.listen({ port: 5000})
+    })
+
+
     .then(res =>{
         console.log(`Server running at ${res.url}`)
     })
