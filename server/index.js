d3fe27fdc8776b9ad1e1f1965cb2169beefe20de
VinjitR <sregulag@usc.edu>
2023-11-03 23:30:02 -0700

    User Registration and validation

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const { MONGODB } = require('./config.js');
 const typeDefs = require('./graphql/typeDefs.js');
-const resolvers =  require('./graphql/resolvers/index.js');
+const resolvers =  require('./graphql/resolvers');
 
 const server = new ApolloServer({
     typeDefs,
