9d44c0eb13b9f8196aba2a6de5441b3460f801b4
VinjitR <sregulag@usc.edu>
2023-11-17 00:29:30 -0800

    posts page, navbar for the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
 
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>RandomTaps</h1>
-    </div>
+    <Router>
+      <Container fluid >
+      <MenuBar/>
+      <Routes>
+      <Route path="/" element={ <Home/> }/>
+      <Route path='login' element={ <Login/> }/>
+      <Route path='register' element={ <Register/> }/>
+      </Routes>
+      </Container>
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,7 @@ import ApolloClientProvider from './ApolloClientProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <ApolloClientProvider />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
