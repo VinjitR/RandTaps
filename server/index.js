06b37c6c7103a555ed6acc1bba8c0e5550134f5b
VinjitR <sregulag@usc.edu>
2023-12-02 21:38:20 -0800

    Deploy Server

--- server/index.js
+++ server/index.js
@@ -9,6 +9,8 @@ const resolvers =  require('./graphql/resolvers');
 
 const pubsub= new PubSub();
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -18,8 +20,11 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, {useNewUrlParser: true})
     .then(()=>{
         console.log("MongoDB connected")
-        return server.listen({ port: 5000})
+        return server.listen({ port: PORT})
     })
     .then(res =>{
         console.log(`Server running at ${res.url}`)
+    })
+    .catch(err =>{
+        console.error(err);
     })
